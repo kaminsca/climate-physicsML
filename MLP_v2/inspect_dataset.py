6eff75f77c2f5765b2458a62470fce38e732c174
Clark Kaminsky <clarkkam@gl-login2.arc-ts.umich.edu>
2024-11-14 14:43:33 -0500

    figured out more abt  stacking / variables

--- MLP_v2/inspect_dataset.py
+++ MLP_v2/inspect_dataset.py
@@ -1,13 +1,13 @@
 import xarray as xr
 import glob, os
 import random
-from energy_loss import total_loss
 import tensorflow as tf
 import logging
+import numpy as np
 
 # Configure logging
-# logging.basicConfig(level=logging.DEBUG)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 CLIMSIM_PATH = '/home/clarkkam/AIFS/ClimSim/'
@@ -42,11 +42,11 @@ def load_nc_dir_with_generator(filelist:list):
             
             # read mlo
             dso = xr.open_dataset(file.replace('.mli.','.mlo.'), engine='netcdf4')
+            logger.debug(f"cam_in_LWUP pre stack: {ds['cam_in_LWUP'][0].values}")
             out_vars = list(dso.keys())
             unused_out_vars = [x for x in out_vars if x not in vars_mlo]
             logger.debug("USED OUTPUT VARIABLES %s", vars_mlo)
             logger.debug("UNUSED OUTPUT VARIABLES %s", unused_out_vars)
-            # dso.info()
             
             # make mlo variales: ptend_t and ptend_q0001
             dso['ptend_t'] = (dso['state_t'] - ds['state_t'])/1200 # T tendency [K/s]
@@ -55,7 +55,7 @@ def load_nc_dir_with_generator(filelist:list):
             logger.debug(f"Read mlo from {file.replace('.mli.', '.mlo.')}. Dimensions: {dso.dims}")
             # dso.info()
 
-            # normalizatoin, scaling
+            # normalization, scaling
             ds = (ds-mli_mean)/(mli_max-mli_min)
             dso = dso*mlo_scale
             logger.debug("Normalized and scaled datasets.")
@@ -64,22 +64,44 @@ def load_nc_dir_with_generator(filelist:list):
             # stack
             #ds = ds.stack({'batch':{'sample','ncol'}})
             ds = ds.stack({'batch':{'ncol'}})
-            # logger.debug(f"Stacked mli dataset. Dimensions: {ds.dims}")
             ds = ds.to_stacked_array("mlvar", sample_dims=["batch"], name='mli')
             logger.debug(f"Converted mli dataset to stacked array. Shape: {ds.shape}")
-            logger.debug(f"0-59: state_t, 60-119: state_q0001, 120: state_ps, 121: pbuf_SOLIN, 122: pbuf_LHFLX, 123: pbuf_SHFLX, 124: cam_in_LWUP")
 
             dso = dso.stack({'batch': {'ncol'}})
-            # logger.debug(f"Stacked mlo dataset. Dimensions: {dso.dims}")
             dso = dso.to_stacked_array("mlvar", sample_dims=["batch"], name='mlo')
             logger.debug(f"Converted mlo dataset to stacked array. Shape: {dso.shape}")
-            logger.debug(f"0-59: ptend_t, 60-119: ptend_q0001, 120: cam_out_NETSW, 121: cam_out_FLWDS, 122: cam_out_PRECSC, 123: cam_out_PRECC, 124: cam_out_SOLS, 125: cam_out_SOLL, 126: cam_out_SOLSD, 127: cam_out_SOLLD")
 
+            # logger.debug(f"Variables in ds_stacked.mlvar: {ds.mlvar.values}")
+            # logger.debug(f"Variables in dso_stacked.mlvar: {dso.mlvar.values}")
+            
+            def match_var(variable, mlvar_array):
+                indices = []
+                for i, v in enumerate(mlvar_array):
+                    if isinstance(v, tuple) and v[0] == variable:
+                        indices.append(i)
+                return indices
+            
+            def find_indices_for_variables(variables, mlvar_array):
+                variable_indices = {}
+                for var in variables:
+                    indices = match_var(var, mlvar_array)
+                    variable_indices[var] = indices
+                    logger.debug(f"{var}: {indices}")
+                return variable_indices
+
+            logger.debug("Indices for mli variables:")
+            mlvar_values_ds = ds.mlvar.values
+            indices_mli = find_indices_for_variables(vars_mli, mlvar_values_ds)
+            # Find indices for all variables in dso.mlvar
+            logger.debug("Indices for mlo variables:")
+            mlvar_values_dso = dso.mlvar.values
+            indices_mlo = find_indices_for_variables(vars_mlo, mlvar_values_dso)
+            # These two should be the same:
+            logger.debug(f"cam_in_LWUP[0]: {ds.sel(mlvar=('cam_in_LWUP'))[0].values}")
+            logger.debug(f"cam_in_LWUP[0]: {ds[0, 124].values}")
 
             logger.debug("Shape of ds.values: %s", ds.values.shape)
             logger.debug("Shape of dso.values: %s", dso.values.shape)
-            # print("Example of ds.values:", ds.values[:1])
-            # print("Example of dso.values:", dso.values[:1])
             
             yield (ds.values, dso.values)
 
@@ -101,77 +123,73 @@ print(f'[TRAIN] Total # of input files: {len(f_mli)}')
 print(f'[TRAIN] Total # of columns (nfiles * ncols): {len(f_mli)*384}')
 tds = load_nc_dir_with_generator(f_mli)
 tds = tds.unbatch()
-tds = tds.shuffle(buffer_size=shuffle_buffer, reshuffle_each_iteration=True)
-tds = tds.prefetch(buffer_size=4) # in realtion to the batch size
 
-
-def manual_count(dataset):
-    count = 0
-    for _ in dataset:
-        count += 1
-    return count
-print(f"Length of the dataset: {manual_count(tds)}")
 
 iterator = iter(tds)
 # curious about cam_in_LWUP from input and cam_out_SOLS / SOLSD / etc from output:
 x, y = next(iterator)
 print(x[124])
-print(y[124])
-print(y[124:128])
-count_non_zero = 0
-try:
-    while True:
-        x, y = next(iterator)
-        # Check if any element in y[124:128] is not 0
-        if tf.reduce_any(y[124:128] != 0).numpy():
-            count_non_zero += 1
-            # print("y[124:128]:", y[124:128])
-except StopIteration:
-    pass
-print(count_non_zero, 'non zero values in cam_out_SOLS / SOLSD / ...')
+# print(y[124])
+# print(y[124:128])
+# count_non_zero = 0
+# total = 0
+# try:
+#     while True:
+#         x, y = next(iterator)
+#         # Check if any element in y[124:128] is not 0
+#         if tf.reduce_any(y[124:128] != 0).numpy():
+#             count_non_zero += 1
+#             # print("y[124:128]:", y[124:128])
+#         total += 1
+# except StopIteration:
+#     pass
+# print(count_non_zero, 'non zero values in cam_out_SOLS / SOLSD / ... out of ', total)
 
-# test energy loss
-iterator = iter(tds)
-counter = 0
-variable_contributions = {
-    'r_out': 0,
-    'lh': 0,
-    'sh': 0,
-    'r_in' : 0,
-    'SOLS' : 0,
-    'SOLL' : 0,
-    'SOLSD' : 0,
-    'SOLLD' : 0
-}
-try:
-    while True:
-        x, y = next(iterator)
-        # r_out = upward longwave flux 
-        # Surface latent heat flux (LH), and Surface sensible heat flux (SH)
-        r_out = x[124]
-        lh = x[122]
-        sh = x[123]
-        # r_in = visible direct flux + near-IR direct flux + visible diffuse flux + near-IR diffuse flux
-        r_in = y[124] + y[125] + y[126] + y[127]
+# # test energy loss
+# iterator = iter(tds)
+# counter = 0
+# variable_contributions = {
+#     'r_out': 0,
+#     'lh': 0,
+#     'sh': 0,
+#     'r_in' : 0,
+#     'SOLS' : 0,
+#     'SOLL' : 0,
+#     'SOLSD' : 0,
+#     'SOLLD' : 0
+# }
+# loss_sum = 0
+# try:
+#     while True:
+#         x, y = next(iterator)
+#         # r_out = upward longwave flux 
+#         # Surface latent heat flux (LH), and Surface sensible heat flux (SH)
+#         r_out = x[124]
+#         lh = x[122]
+#         sh = x[123]
+#         # r_in = visible direct flux + near-IR direct flux + visible diffuse flux + near-IR diffuse flux
+#         r_in = y[124] + y[125] + y[126] + y[127]
 
-        variable_contributions['r_out'] += abs(r_out)
-        variable_contributions['lh'] += abs(lh)
-        variable_contributions['sh'] += abs(sh)
-        variable_contributions['r_in'] += abs(r_in)
-        variable_contributions['SOLS'] += abs(y[124])
-        variable_contributions['SOLL'] += abs(y[125])
-        variable_contributions['SOLSD'] += abs(y[126])
-        variable_contributions['SOLLD'] += abs(y[127])
+#         variable_contributions['r_out'] += abs(r_out)
+#         variable_contributions['lh'] += abs(lh)
+#         variable_contributions['sh'] += abs(sh)
+#         variable_contributions['r_in'] += abs(r_in)
+#         variable_contributions['SOLS'] += abs(y[124])
+#         variable_contributions['SOLL'] += abs(y[125])
+#         variable_contributions['SOLSD'] += abs(y[126])
+#         variable_contributions['SOLLD'] += abs(y[127])
 
-        # Lec = r_in − (r_out + lh + sh)
-        loss_ec = r_in - (r_out + lh + sh)
-        # print(f"EC Loss[{counter}]: {loss_ec}")
-        # print(f"        r_in = {r_in}, r_out = {r_out}, lh = {lh}, sh = {sh}")
-        counter += 1
-except StopIteration:
-    pass
+#         # Lec = r_in − (r_out + lh + sh)
+#         loss_ec = r_in - (r_out + lh + sh)
+#         loss_sum += loss_ec
+#         # print(f"EC Loss[{counter}]: {loss_ec}")
+#         # print(f"        r_in = {r_in}, r_out = {r_out}, lh = {lh}, sh = {sh}")
+#         counter += 1
+# except StopIteration:
+#     pass
 
-average_contributions = {key: value / counter for key, value in variable_contributions.items()}
-print("Average contributions to loss:")
-for var, contrib in average_contributions.items():
-    print(f"{var}: {contrib}")
+# print(f"avg loss: {loss_sum / counter}")
+# average_contributions = {key: value / counter for key, value in variable_contributions.items()}
+# print("Average contributions to loss:")
+# for var, contrib in average_contributions.items():
+#     print(f"{var}: {contrib}")
